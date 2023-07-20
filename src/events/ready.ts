e5775387c4342cc941b4f02a306be992b8f99594
Naomi Carrigan <nhcarrigan@gmail.com>
2023-07-20 15:29:13 -0700

    feat: remove outfit of the day feature

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,8 +1,6 @@
 import { Client, REST, Routes } from "discord.js";
-import { scheduleJob } from "node-schedule";
 
 import { Commands } from "../config/Commands";
-import { getOutfit } from "../modules/getOutfit";
 import { errorHandler } from "../utils/errorHandler";
 import { logHandler } from "../utils/logHandler";
 
@@ -22,22 +20,6 @@ export const ready = async (bot: Client) => {
 
     await rest.put(Routes.applicationCommands(bot.user?.id || "oopsie"), {
       body: Commands,
-    });
-    // run daily at 7AM
-    scheduleJob("0 0 7 * * *", async () => {
-      const guild = bot.guilds.resolve("778130114772598785");
-      if (!guild) {
-        return;
-      }
-      const channel = guild.channels.resolve("1070194415013994506");
-      if (!channel || !("send" in channel)) {
-        return;
-      }
-      const outfit = await getOutfit();
-      await channel.send({
-        content: "Here is <@!465650873650118659>'s outfit of the day~!",
-        embeds: [outfit],
-      });
     });
   } catch (err) {
     await errorHandler("ready", err);
